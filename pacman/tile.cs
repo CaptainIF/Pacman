fa0762b919c00af3d0e96301ed23b5a4aa05ae31
JakobHenrysson <jakob.henrysson@telia.com>
2021-03-31 11:23:21 +0200

    Worked more on CalculateWalls

--- pacman/tile.cs
+++ pacman/tile.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -11,7 +13,9 @@ namespace pacman {
         public int tileID;
         Texture2D tileTexture;
         
-        public tile(Game game): base(game) {}
+        public tile(Game game): base(game) {
+            
+        }
         public void Initialize(Vector2 position, int tileID) {
             this.position = position;
             this.tileID = tileID;
@@ -24,22 +28,60 @@ namespace pacman {
         }
 
         public void CalculateWalls(gameMap map) {
-            if(tileID == 1) {
-                Debug.WriteLine(this.position.X.ToString() + ", " + this.position.Y.ToString());
+            if(tileID == 0) {
+                if(CheckWallNeighbours(map)) {
+                    List<tile> neighbours = CheckNeighbours(map);
+                    if(neighbours.Count == 3) {
+                        Debug.WriteLine(neighbours[0].position.X);
+                    }
+                    if(neighbours.Count == 2) {
+
+                    }
+                } else {
+                    this.wallID = 0;
+                }
             }
         }
 
-        public tile[] CheckNeighbours(gameMap map) {
+        public List<tile> CheckNeighbours(gameMap map) {
             tile[,] grid = map.tiles;
             int i = (int)this.position.X;
             int j = (int)this.position.Y;
-            tile[] result;
-            if(grid[i, j].tileID == 1) {
-                result[result.Length] = grid[i, j];
+            List<tile> neighbours = new List<tile>();
+            if((j - 1) >= 0 && grid[i, j - 1].tileID == 0) {
+                neighbours.Add(grid[i, j - 1]);
+            }
+            if((i + 1) < map.width && grid[i + 1, j].tileID == 0) {
+                neighbours.Add(grid[i + 1, j]);
+            }
+            if((j + 1) < map.height && grid[i, j + 1].tileID == 0) {
+                neighbours.Add(grid[i, j + 1]);
+            }
+            if((i - 1) >= 0 && grid[i - 1, j].tileID == 0) {
+                neighbours.Add(grid[i - 1, j]);
             }
 
 
-            return result;
+            return neighbours;
+        }
+
+        public bool CheckWallNeighbours(gameMap map) {
+            tile[,] grid = map.tiles;
+            int i = (int)this.position.X;
+            int j = (int)this.position.Y;
+            if ((j - 1) >= 0 && grid[i, j - 1].tileID == 1) {
+                return true;
+            }
+            if ((i + 1) < map.width && grid[i + 1, j].tileID == 1) {
+                return true;
+            }
+            if ((j + 1) < map.height && grid[i, j + 1].tileID == 1) {
+                return true;
+            }
+            if ((i - 1) >= 0 && grid[i - 1, j].tileID == 1) {
+                return true;
+            }
+            return false;
         }
 
         public void LoadTexture() {
@@ -59,7 +101,7 @@ namespace pacman {
         public gameMap(Game game,int width, int height):base(game) {
             this.width = width;
             this.height = height;
-            tiles = new tile[28, 36];
+            tiles = new tile[this.width, this.height];
 
            
             using (StringReader testSR = new StringReader(Properties.Resources.pacmapText)) {
@@ -82,6 +124,7 @@ namespace pacman {
                     tiles[i, j].CalculateWalls(this);
                 }
             }
+            //Debug.WriteLine(tiles[2, 6].CheckNeighbours(this).Count);
         }
 
         public override void Draw(GameTime gt) {
