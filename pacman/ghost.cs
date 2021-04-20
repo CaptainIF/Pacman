ae8a1f0bd83dfda232d81efe7ad4a72d6c16fc53
Isak Fröberg <isakfroberg@mail.com>
2021-04-20 13:58:36 +0200

    Update ghost.cs

--- pacman/ghost.cs
+++ pacman/ghost.cs
@@ -13,7 +13,8 @@ namespace pacman {
         public Vector2 pos;
         public int width = 50;
         public int currentI, currentJ;
-        public ghost(Game game, int i, int j,int speed):base(game) {
+        public gameMap map;
+        public ghost(Game game, int i, int j,int speed, pacman torsten, gameMap map):base(game) {
 
             //tile1 Y - pacman Y = katet1[1]
             //tile2 Y - pacman Y = katet2[1]
@@ -21,10 +22,27 @@ namespace pacman {
             //tile2 X - pacman X = katet2[2]
 
             //compare hypotenuse choose tile.
+            //i kolumn
+            //j kolumn
 
             this.speed = speed;
-
+            this.pos.X = i * 28 + 14;
+            this.pos.Y = j * 28 + 14;
+            this.dir.X = 0;
+            this.dir.Y = 0;
         }
 
+        public void update() {
+
+            this.currentI = (int)((this.pos.X) / map.tiles[0, 0].size);
+            this.currentJ = (int)((this.pos.Y) / map.tiles[0, 0].size);
+
+            map.tiles[currentI, currentJ].CheckNeighbours(map);
+
+            
+
+            
+
+        }
     }
 }
