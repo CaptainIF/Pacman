d6240af4bb8ab4b550990310405b52c078d89444
Isak Fröberg <isakfroberg@mail.com>
2021-04-28 11:19:58 +0200

    Update ghost.cs

--- pacman/ghost.cs
+++ pacman/ghost.cs
@@ -14,26 +14,14 @@ namespace pacman {
         public int width = 50;
         public int currentI, currentJ;
         public Texture2D texture;
-       
-
 
         public ghost(Game game, int i, int j,int speed):base(game) {
-
-            //tile1 Y - pacman Y = katet1[1]
-            //tile2 Y - pacman Y = katet2[1]
-            //tile1 X - pacman X = katet1[2]
-            //tile2 X - pacman X = katet2[2]
-
-            //compare hypotenuse choose tile.
-            //i kolumn
-            //j kolumn
 
             this.speed = speed;
             this.pos.X = i * 28 + 14;
             this.pos.Y = j * 28 + 14;
             this.dir.X = -1;
             this.dir.Y = 0;
-
         }
 
         public void Update(gameMap map, pacman torsten) {
@@ -41,79 +29,62 @@ namespace pacman {
             this.currentJ = (int)((this.pos.Y) / map.tiles[0, 0].size);
             var kstate = Keyboard.GetState();
             var neighbour = map.tiles[currentI, currentJ].CheckNeighbours(map);
-            
-            Debug.WriteLine(neighbour.Count);
 
+            if (neighbour.Count == 2 || neighbour.Count == 1 && this.pos.X % 28 < 14 + this.speed / 2 && this.pos.X % 28 > 14 - this.speed / 2) {
 
-            /*if (kstate.IsKeyDown(Keys.W)) {
-                if ((map.tiles[this.currentI, this.currentJ - 1].tileID == 1 || map.tiles[this.currentI, this.currentJ - 1].tileID == 2) && this.pos.X % 28 < 14 + this.speed / 2 && this.pos.X % 28 > 14 - this.speed / 2) {
+                //Debug.WriteLine(map.tiles[currentI, currentJ].tileID);
+                
+
+                if ((map.tiles[this.currentI + 1, this.currentJ].tileID == 0 || map.tiles[this.currentI - 1, this.currentJ].tileID == 0) && map.tiles[this.currentI, this.currentJ + 1].tileID == 0 && this.dir.X == 0) {
                     this.dir.X = 0;
                     this.dir.Y = -1;
+
+                    Debug.WriteLine("UP");
                 }
-            }
-            if (kstate.IsKeyDown(Keys.S)) {
-                if ((map.tiles[this.currentI, this.currentJ + 1].tileID == 1 || map.tiles[this.currentI, this.currentJ + 1].tileID == 2) && this.pos.X % 28 < 14 + this.speed / 2 && this.pos.X % 28 > 14 - this.speed / 2) {
+                else if ((map.tiles[this.currentI + 1, this.currentJ].tileID == 0 || map.tiles[this.currentI - 1, this.currentJ].tileID == 0) && map.tiles[this.currentI, this.currentJ - 1].tileID == 0 && this.dir.X == 0) {
                     this.dir.X = 0;
                     this.dir.Y = 1;
+
+                    Debug.WriteLine("DOWN");
+
                 }
-            }
-            if (kstate.IsKeyDown(Keys.A)) {
-                if ((map.tiles[this.currentI - 1, this.currentJ].tileID == 1 || map.tiles[this.currentI - 1, this.currentJ].tileID == 2) && this.pos.Y % 28 < 14 + this.speed / 2 && this.pos.Y % 28 > 14 - this.speed / 2) {
+
+
+                else if ((map.tiles[this.currentI, this.currentJ + 1].tileID == 0 || map.tiles[this.currentI, this.currentJ - 1].tileID == 0) && (map.tiles[this.currentI + 1, this.currentJ - 1].tileID == 0 || map.tiles[this.currentI + 1, currentJ + 1].tileID == 0) && this.dir.Y == 0) {
                     this.dir.X = -1;
                     this.dir.Y = 0;
+
+                    Debug.WriteLine("LEFT");
+
                 }
-            }
-            if (kstate.IsKeyDown(Keys.D)) {
-                if ((map.tiles[this.currentI + 1, this.currentJ].tileID == 1 || map.tiles[this.currentI + 1, this.currentJ].tileID == 2) && this.pos.Y % 28 < 14 + this.speed / 2 && this.pos.Y % 28 > 14 - this.speed / 2) {
+
+
+                else if ((map.tiles[this.currentI + 1, this.currentJ].tileID == 0 || map.tiles[this.currentI - 1, this.currentJ].tileID == 0) && (map.tiles[this.currentI - 1, this.currentJ - 1].tileID == 0 || map.tiles[this.currentI - 1, currentJ + 1].tileID == 0) && this.dir.Y == 0) {
                     this.dir.X = 1;
                     this.dir.Y = 0;
+
+                    Debug.WriteLine("RIGHT");
+
                 }
-            }*/
+            }
 
-            /*if (neighbour.Count == 2) {
-
-                if ((map.tiles[this.currentI, this.currentJ - 1].tileID == 1 || map.tiles[this.currentI, this.currentJ - 1].tileID == 2) && this.pos.X % 28 < 14 + this.speed / 2 && this.pos.X % 28 > 14 - this.speed / 2) {
+                if (map.tiles[(int)((this.pos.X + (this.dir.X * 14)) / 28), (int)((this.pos.Y + this.dir.Y * 14) / 28)].tileID == 1
+                    || map.tiles[(int)((this.pos.X + (this.dir.X * 14)) / 28), (int)((this.pos.Y + this.dir.Y * 14) / 28)].tileID == 2) {
+                    this.pos.X += this.dir.X * this.speed;
+                    this.pos.Y += this.dir.Y * this.speed;
+                //Debug.WriteLine("keep going");
+                }
+                else {
                     this.dir.X = 0;
-                    this.dir.Y = -1;
-                }
-
-
-                else if ((map.tiles[this.currentI, this.currentJ + 1].tileID == 1 || map.tiles[this.currentI, this.currentJ + 1].tileID == 2) && this.pos.X % 28 < 14 + this.speed / 2 && this.pos.X % 28 > 14 - this.speed / 2) {
-                    this.dir.X = 0;
-                    this.dir.Y = 1;
-                }
-
-
-                else if ((map.tiles[this.currentI - 1, this.currentJ].tileID == 1 || map.tiles[this.currentI - 1, this.currentJ].tileID == 2) && this.pos.Y % 28 < 14 + this.speed / 2 && this.pos.Y % 28 > 14 - this.speed / 2) {
-                    this.dir.X = -1;
                     this.dir.Y = 0;
-                }
-
-
-                else if ((map.tiles[this.currentI + 1, this.currentJ].tileID == 1 || map.tiles[this.currentI + 1, this.currentJ].tileID == 2) && this.pos.Y % 28 < 14 + this.speed / 2 && this.pos.Y % 28 > 14 - this.speed / 2) {
-                    this.dir.X = 1;
-                    this.dir.Y = 0;
-                }
-            }
-
-            if (map.tiles[(int)((this.pos.X  + (this.dir.X * 14)) / 28), (int)((this.pos.Y + this.dir.Y * 14) / 28)].tileID == 1 || map.tiles[(int)((this.pos.X  + (this.dir.X * 14)) / 28), (int)((this.pos.Y + this.dir.Y * 14) / 28)].tileID == 2) {
-                this.pos.X += this.dir.X * this.speed;
-                this.pos.Y += this.dir.Y * this.speed;
-            }
-            else {
-                this.dir.X = 0;
-                this.dir.Y = 0;
-                this.pos.X = this.currentI * 28 + 14;
-                this.pos.Y = this.currentJ * 28 + 14;
-            }
-            */
-        } 
+                    this.pos.X = this.currentI * 28 + 14;
+                    this.pos.Y = this.currentJ * 28 + 14;
+                }        
+        }
 
         public void Draw(GameTime gt) {
             SpriteBatch sb = new SpriteBatch(GraphicsDevice);
             this.texture = Game.Content.Load<Texture2D>("spöke_1");
-            
-
 
             sb.Begin();
             sb.Draw(this.texture, new Rectangle((int)this.pos.X - this.width / 2, (int)this.pos.Y - this.width / 2, this.width, this.width), Color.White);
